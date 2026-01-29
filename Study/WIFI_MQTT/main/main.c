ab04176a309dfbac671d06b986f18a5da1087c6e
lxl-git05 <XinLiu051024@outlook.com>
2026-01-29 22:05:09 +0800

    新增Initial模块，精简程序

--- Study/WIFI_MQTT/main/main.c
+++ Study/WIFI_MQTT/main/main.c
@@ -1,39 +1,20 @@
-#include <stdio.h>
-#include <esp_log.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"
-#include "stdbool.h"
+#include "Initial.h"
 
-#include "Timer_Counter.h"
-
-#include "MyLED.h"
-#include "OLED.h"
-#include "Timer.h"
-#include "Key.h"
-#include "PWM.h"
-#include "Wifi_Manager.h"   // WIFI
-#include "Wifi_MQTT.h"
-
+// WIFI模式下的需要外部声明的参数
 char Command[64] = {0} ;
 int LED_PWM = 0 ;
 bool LED_Status ;
 
 void app_main(void)
 {
-    OLED_Init();
-    LED_Init();
-    Timer_Init();
-    Key_Init();
-    PWM_Init();
-    Wifi_Init();    // WIFI
-    
+    Initial() ;
+
     while (1)
     {
         Timer_Counter_Func() ;
         //OLED显示
         OLED_Printf(0 , 0 , OLED_8X16 , "%s" , "Hello World");
-        // Key
+        // Key输入
         if(Key_Check(KEY_0 , KEY_SINGLE))
         {
             printf("Key_0 SINGLE\n");
@@ -47,11 +28,13 @@ void app_main(void)
             Timer_Counter_Print();
             Wifi_Reconnect();
         }
+        // LED
+        LED_Write(LED_Status);
+        PWM_Set_Duty_1024(LED_PWM , PWM_Channel_0);
         // 功能计时区
         Timer_Counter_Begin();
 
-        LED_Write(LED_Status);
-        PWM_Set_Duty_1024(LED_PWM , PWM_Channel_0);
+        
 
         Timer_Counter_End();
         
@@ -62,15 +45,6 @@ void app_main(void)
 // 定时器1ms中断
 void Timer_Callback_1ms(void)
 {
-    static int tim_cnt = 0 ;
-    tim_cnt ++ ;
-
-    // 功能1: 1s
-    if (tim_cnt >= 1000)
-    {
-        tim_cnt = 0 ;
-    }
-
-    // 功能2:检测按键状态1ms周期
+    // 功能1:检测按键状态:1ms周期
     Key_Tick();
 }

b4bbb4db3fa150660cc11b7d687967b81a214ed4
lxl-git05 <XinLiu051024@outlook.com>
2026-01-31 13:17:42 +0800

    MPU6050实现互补滤波

--- Study/Timer/main/main.c
+++ Study/Timer/main/main.c
@@ -8,34 +8,47 @@
 #include "Timer.h"
 #include "Timer_Counter.h"
 #include "MPU6050.h"
-
-int check = 0 ;
+#include "MPU6050_Angle.h"
 
 void app_main(void)
 {
     OLED_Init();
     LED_Init();
+    MPU6050_Init() ;                    // 初始化MPU6050
+    MPU6050_Data_Error_Check(1000) ;    // 纠正零漂
     Timer_Init();
-    MPU6050_Init() ;    // 初始化MPU6050
-    // MPU6050_Data_Error_Check() ;    // 纠正零漂
     while (1)
     {
-        //OLED
-        OLED_ShowChar(0, 0, check ++, OLED_8X16);
-        
-        // MPU6050更新
-        Timer_Counter_Begin() ;
+        Timer_Counter_Func() ;
+        //OLED展示参数
+        OLED_ShowFloatNum( 0 ,  0 , MPU_Real.AccX , 3 , 3 , OLED_6X8 ) ;
+        OLED_ShowFloatNum( 0 , 20 , MPU_Real.AccY , 3 , 3 , OLED_6X8 ) ;
+        OLED_ShowFloatNum( 0 , 40 , MPU_Real.AccZ , 3 , 3 , OLED_6X8 ) ;
 
+        OLED_ShowFloatNum(60 ,  0 , MPU_Real.pitch , 3 , 3 , OLED_6X8 ) ;
+        OLED_ShowFloatNum(60 , 20 , MPU_Real.roll  , 3 , 3 , OLED_6X8 ) ;
+        OLED_ShowFloatNum(60 , 40 , MPU_Real.yaw   , 3 , 3 , OLED_6X8 ) ;
         MPU6050_Update_Data() ;
+        // MPU6050更新
+        Timer_Counter_Begin() ; // ============================ 计时开始
 
-        Timer_Counter_End();
-        printf("Timer Counter:%lld\n" , time_us) ;
-        ESP_LOGI("main", "conut: %d", check);
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        // 功能: 得到积分角度,但是时间不精确
+        MPU6050_Raw_Error_Update();	// 更新去零参数
+        static int MPU_Count  = 0 ;
+        MPU_Count ++ ;
+        if (MPU_Count == 10) // 10ms
+        {
+            MPU_Count = 0 ;
+            MPU6050_Raw_Deal(10) ;	// 10ms更新
+        }
+
+        Timer_Counter_End();    // ============================ 计时结束
+
+        printf("Timer Counter:%lld While Timer:%lld\n" , time_us , time_Func_us) ;
 
         // MPU6050静止检测 + 自动调节零漂
-        // MPU_Still_Check() ;
-		// MPU6050_Data_Error_Check_Auto() ;
+        MPU_Still_Check() ;
+		MPU6050_Data_Error_Check_Auto() ;
 
         OLED_Update();
     }
@@ -55,14 +68,4 @@ void Timer_Callback_1ms(void)
         tim_cnt = 0 ;
         LED_Write(led_Status);
     }
-
-    // 功能2 : 得到积分角度
-	// MPU6050_Raw_Error_Update();	// 更新去零参数
-	// static int MPU_Count  = 0 ;
-	// MPU_Count ++ ;
-	// if (MPU_Count == 10) // 10ms
-	// {
-	// 	MPU_Count = 0 ;
-	// 	MPU6050_Raw_Deal(10) ;	// 10ms更新
-	// }
 }

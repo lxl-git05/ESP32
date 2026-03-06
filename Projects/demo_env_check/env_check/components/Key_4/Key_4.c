6d8d628931dc856378c7a678ef87fa09e94ab4c4
lxl-git05 <XinLiu051024@outlook.com>
2026-03-06 18:49:05 +0800

    完成按键逻辑的完善

--- Projects/demo_env_check/env_check/components/Key_4/Key_4.c
+++ Projects/demo_env_check/env_check/components/Key_4/Key_4.c
@@ -5,59 +5,82 @@
 #include "Timer_Counter.h"
 
 // #define Key_Debug
-Key_Status_Typedef Key_Status ;
+Key_Status_Typedef Key_S ;
 
 // ====================== 外部任务 ======================
 
-// 4按键传感器
+// 4按键传感器初始化
 void Key_4_Init(void)
 {
     adc_init(Key_Channel) ;
 }
 
+// 得到ADC的值
 void Key_4_Get(int *Key_Data)
 {
     ADC_Read_Raw(Key_Data , Key_Channel) ;
 }
 
+// 得到按键按下or没按下的状态
 Key_Status_Typedef Key_4_Get_Status(void)
 {
-    return Key_Status ;
+    Key_Status_Typedef temp = Key_S ;
+    Key_S = Key_NULL ;
+    return temp ;
 }
 
+// ADC检测任务
 // =========== Task : 四按键模块数据读取(20ms) ============ 
 void Task_Key4(void *param)
 {
-    // setup: ADC初始化
-    Key_4_Init() ;
-    int key_data = 0 ;
+    Key_4_Init();
+
+    int key_data = 0;
+    Key_Status_Typedef Key_Status ;
+    Key_Status_Typedef last_status = Key_NULL;   // 上一次状态
+    uint8_t key_lock = 0;
+
     while (1)
     {
-        Key_4_Get(&key_data) ;
+        Key_4_Get(&key_data);
+
+        // ====== 判断当前按键 ======
         if (key_data > 3000)
         {
-            Key_Status = KEY_4_Single ;
+            Key_Status = KEY_4_Single;
         }
         else if (key_data > 2600 && key_data < 3000)
         {
-            Key_Status = KEY_3_Single ;
+            Key_Status = KEY_3_Single;
         }
         else if (key_data > 2300 && key_data < 2600)
         {
-            Key_Status = KEY_2_Single ;
+            Key_Status = KEY_2_Single;
         }
         else if (key_data > 2000 && key_data < 2250)
         {
-            Key_Status = KEY_1_Single ;
+            Key_Status = KEY_1_Single;
         }
-        else 
+        else
         {
-            Key_Status = Key_NULL ;
-        }     
-        #ifdef Key_Debug 
-        printf("key_status = %d\n" , Key_Status) ;
-        #endif
-        vTaskDelay(pdMS_TO_TICKS(20)) ;
+            Key_Status = Key_NULL;
+        }
+
+        // ====== 边沿检测 ======
+        
+
+        if (Key_Status != Key_NULL && key_lock == 0)
+        {
+            key_lock = 1;
+            printf("Key Pressed\n");
+            Key_S = Key_Status ;
+        }
+
+        if (Key_Status == Key_NULL)
+        {
+            key_lock = 0;
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(20));
     }
 }
-
--- Projects/demo_env_check/env_check/components/Key_4/Key_4.h
+++ Projects/demo_env_check/env_check/components/Key_4/Key_4.h
@@ -14,12 +14,10 @@ typedef enum
 
 // =============== 数据层 ===============
 #define Key_Channel  ADC_CHANNEL_0   // ADC_CHANNEL_0 -> GPIO1 - D0
-extern Key_Status_Typedef Key_Status ;
 // =============== 数据层 ===============
 
 // 4按键传感器
 void Key_4_Init(void);
-void Key_4_Get(int *Gray_Data);
 Key_Status_Typedef Key_4_Get_Status(void) ;
 // 任务
 void Task_Key4(void *param) ;
